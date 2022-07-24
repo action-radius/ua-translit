03357f8e69556445ab2770ee6f313d7bf2eb1a99
action-radius <art.fedotov.ua@gmail.com>
2022-07-24 20:39:55 +0200

    Icons, buttons and two smoking barrels. Moved buttons, labels, text-boxes. Added Icon to 'copy' button.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import translit
 from tkinter import *
 import os
 # from subprocess import Popen, PIPE
@@ -7,13 +8,8 @@ os.system('clear')
 print("UA - Translit V1.0")
 print("Hello, World! Testing GitHub.")
 
-def copy_button():
-    text = text2.get()
-    # pyperclip.copy(text)
-    print(f'Copied: {text}')
 
-# Not done!
-def translate():
+def copy_button():
     text = text2.get()
     # pyperclip.copy(text)
     print(f'Copied: {text}')
@@ -32,41 +28,47 @@ window.title("Translit")
 window.config(background="white")
 
 label = Label(window, 
-                text="Транслітерація з кирилиці на латиницю", 
+                text="Транслітерація з кирилиці на латиницю.", 
                 font=('Cantarell', 14, 'normal'), 
                 fg='black', 
                 bg='white')
-label.place(x=5, y=5)
+label.place(x=8, y=5)
 
 text = Text(window, 
                 font=('Cantarell', 14, 'normal'),
                 fg="black",
                 bg="white")
-text.place(x=15, y=45, width=700, height=120)
+text.place(x=25, y=45, width=747, height=120)
 
 text2 = Text(window, 
                 font=('Cantarell', 14, 'normal'),
                 fg="black",
                 bg="white",
                 state="disabled")
-text2.place(x=15, y=175, width=700, height=120)
+text2.place(x=25, y=175, width=747, height=120)
 
 translate_button = Button(window, 
-                text="convert",
-                command=translate,
-                font=('Cantarell', 14, 'italic'),
-                fg="black",
-                bg="#e3e3e3")
-translate_button.place(x=85, y=305, width=80, height=38)
-translate_button["border"] = "0"
+                font=('Cantarell', 14, 'normal'),
+                command=None,
+                bg="#e3e3e3",
+                text="Convert",
+                fg="black")
+translate_button.place(x=122, y=308, width=80, height=38)
+translate_button["border"] = "1"
+
+# Icon
+photo = PhotoImage(file = r"/home/artemiy/git_workspace/ua-translit/copy_btn_icon.png")
+photoimage = photo.subsample(5, 4)
 
 copying_button = Button(window, 
-                text="copy",
+                font=('Cantarell', 14, 'normal'),
                 command=copy_button,
-                font=('Cantarell', 14, 'italic'),
-                fg="black",
-                bg="#e3e3e3")
-copying_button.place(x=15, y=305, width=60, height=38)
-copying_button["border"] = "0"
+                image=photoimage,
+                compound=LEFT,
+                bg="#e3e3e3",
+                text="copy",
+                fg="black")
+copying_button.place(x=25, y=308, width=90, height=38)
+copying_button["border"] = "1"
 
 window.mainloop()
