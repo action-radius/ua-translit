01a251fe8aead5d2cf9a08a75ffb82bc87446271
action-radius <art.fedotov.ua@gmail.com>
2022-07-24 19:23:14 +0200

    Added variants of Ukrainian latin alphabet. Changed font and window size in program. Added second button for translate cyrillic to latin.

--- sus.py
+++ sus.py
@@ -1,15 +1,20 @@
 from tkinter import *
-# import pyperclip
-# YOY Guys
-from subprocess import Popen, PIPE
 import os
+# from subprocess import Popen, PIPE
+# import pyperclip
 
 os.system('clear')
 print("UA - Translit V1.0")
 print("Hello, World! Testing GitHub.")
 
 def copy_button():
-    text = entry2.get()
+    text = text2.get()
+    # pyperclip.copy(text)
+    print(f'Copied: {text}')
+
+# Not done!
+def translate():
+    text = text2.get()
     # pyperclip.copy(text)
     print(f'Copied: {text}')
 
@@ -17,9 +22,9 @@ def copy_button():
 # windows = serves as a container to hold or contain these widgets
 
 window = Tk()
-window.geometry("800x500")
+window.geometry("800x360")
 window.resizable(0, 0)
-window.title("Translit V1.0")
+window.title("Translit")
 
 # icon = PhotoImage(file='dir_to_icon.png')
 # window.iconphoto(True, icon) # window icon
@@ -28,30 +33,40 @@ window.config(background="white")
 
 label = Label(window, 
                 text="Транслітерація з кирилиці на латиницю", 
-                font=('SF Pro', 20, 'normal'), 
+                font=('Cantarell', 14, 'normal'), 
                 fg='black', 
                 bg='white')
-label.place(x=5, y=0)
+label.place(x=5, y=5)
 
-entry = Entry(window, 
-                font=('SF Pro', 25, 'normal'),
+text = Text(window, 
+                font=('Cantarell', 14, 'normal'),
                 fg="black",
                 bg="white")
-entry.place(x=15, y=40, width=700, height=120)
+text.place(x=15, y=45, width=700, height=120)
 
-entry2 = Entry(window, 
-                font=('SF Pro', 25, 'normal'),
+text2 = Text(window, 
+                font=('Cantarell', 14, 'normal'),
                 fg="black",
-                bg="white")
-entry2.place(x=15, y=170, width=700, height=120)
+                bg="white",
+                state="disabled")
+text2.place(x=15, y=175, width=700, height=120)
 
-submit_button = Button(window, 
-                text="copy",
-                command=copy_button,
-                font=('SF Pro', 24, 'normal'),
+translate_button = Button(window, 
+                text="convert",
+                command=translate,
+                font=('Cantarell', 14, 'italic'),
                 fg="black",
                 bg="#e3e3e3")
-submit_button.place(x=15, y=440, width=80, height=38)
-submit_button["border"] = "0"
+translate_button.place(x=85, y=305, width=80, height=38)
+translate_button["border"] = "0"
+
+copying_button = Button(window, 
+                text="copy",
+                command=copy_button,
+                font=('Cantarell', 14, 'italic'),
+                fg="black",
+                bg="#e3e3e3")
+copying_button.place(x=15, y=305, width=60, height=38)
+copying_button["border"] = "0"
 
 window.mainloop()
---
+++ translit.py
@@ -0,0 +1,121 @@
+# Imports
+from latin.NovaLatynka import NovaLatynka
+from latin.TKPN_combo import TKPN_combo
+from latin.TKPN_diac import TKPN_diac
+from latin.TKPN_intl import TKPN_intl
+from latin.custom import custom
+from latin.iso9 import iso9
+
+vowels = {
+    "а": True,
+    "е": True,
+    "є": True,
+    "и": True,
+    "і": True,
+    "ї": True,
+    "о": True,
+    "у": True,
+    "ю": True,
+    "я": True
+}
+
+forCustom = ["а", "е", "у"]
+for1994diac = ["с", "р", "п", "м", "з", "д", "в", "б"]
+for1994diac_upper = ["С", "Р", "П", "З", "Д", "В", "Б"]
+for1994diac_choice = [4, 5, 6]
+
+
+def translit(message):
+    output = ""
+    isPreviousLetterConsonant = False
+    
+    if choice == "1":
+        lower_dictionary = custom
+    if choice == "2":
+        lower_dictionary = iso9
+    if choice == "3":
+        lower_dictionary = NovaLatynka
+    if choice == "4":
+        lower_dictionary = TKPN_diac
+    if choice == "5":
+        lower_dictionary = TKPN_combo
+    if choice == "6":
+        lower_dictionary = TKPN_intl
+ 
+    for index, i in enumerate(message):
+        if index + 1 != len(message):
+            index += 1
+        lowered = i.lower()
+        l = i
+ 
+        if lowered in lower_dictionary:
+            nextLetter = message[index]
+            l = lower_dictionary[lowered]
+ 
+            ###########################################################
+            if choice == "1":
+                if len(l) == 2 and l[0] == 'j':
+                    if isPreviousLetterConsonant:
+                        l = 'i' + l[1]
+            ###########################################################
+            if choice == "1":
+                if i == "і" and nextLetter in forCustom:
+                    l = "í"
+            ###########################################################
+            if choice in for1994diac_choice:
+                if lowered == "й" and i != message[0]:
+                    if message[index - 2] in for1994diac and nextLetter == "о":
+                        l = "'j"
+
+                if lowered == "й" and i != message[0]:
+                    if message[index - 2] in for1994diac_upper and nextLetter == "О":
+                        l = "'j"
+            ###########################################################
+ 
+            isPreviousLetterConsonant = lowered not in vowels
+            
+ 
+            if message.isupper() and len(message) > 1:
+                 l = l.upper()
+            else:
+                if i.isupper():
+                    if len(message) != 1:
+                        if nextLetter.isupper():
+                            l = l.upper()
+                        if nextLetter.islower() or nextLetter.lower() not in lower_dictionary:
+                            l = l.title()
+                    else: 
+                        l = l.title()
+            if l == "'j" and i.isupper():
+                l = l.upper()
+        else:
+            isPreviousLetterConsonant = False
+        output += l
+    return output
+    
+def test(got, expected):
+    translated = translit(got)
+    if expected != translated:
+        print('transliting "' + got + '": expected "' + expected + '", got "' + translated + '"')
+
+# test("ВЙОМ", "V'JOM")
+# test("бйо вйо дйо зйо мйо пйо рйо сйо", "b'jo v'jo d'jo z'jo m'jo p'jo r'jo s'jo")
+ 
+if __name__ == "__main__":
+    print("1. Custom Latin \n2. ISO9 ГОСТ 7.79 A \n3. Nova Latynka " 
+    "\n4. ТКПН Вакуленко 1994 diac \n5. ТКПН Вакуленко 1994 combo \n6. ТКПН Вакуленко 1994 intl")
+    choice = input("Обиріть латинку: ")
+    message = input("Напишіть кирилицею: ")
+    print(translit(message))
+
+# Моя латинка
+# ISO9 ГОСТ 7.79 А
+# Nova Latynka
+# ТКПН Вакуленко 1994 diac
+# ТКПН Вакуленко 1994 combo
+# ТКПН Вакуленко 1994 intl
+
+#############################
+# Official КМУ 2010         #
+# Avtorśka                  #
+#############################
